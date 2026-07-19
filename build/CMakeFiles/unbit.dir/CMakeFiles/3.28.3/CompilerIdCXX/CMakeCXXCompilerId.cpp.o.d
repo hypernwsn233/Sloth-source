ceaf220a46aedf8711aa19ed540472a412dd68ac
hypernwsn233 <greenxprod@gmail.com>
2026-07-19 17:24:05 -0300

    retirando o build

--- build/CMakeFiles/unbit.dir/CMakeFiles/3.28.3/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/unbit.dir/CMakeFiles/3.28.3/CompilerIdCXX/CMakeCXXCompilerId.cpp.o: \
- /home/pedro/Documentos/Unbit/build/CMakeFiles/3.28.3/CompilerIdCXX/CMakeCXXCompilerId.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/unbit.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for unbit.
-# This may be replaced when dependencies are built.
